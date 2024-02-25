43a739c2721a91cca13417f7438ba5e8844096ba
Malladi Pradyumna <f20210367@hyderabad.bits-pilani.ac.in>
2024-02-25 18:55:35 +0530

    Last fix promise

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-rm -rf obsidian-export
-rm -rf quartz
-git clone https://github.com/mssrprad/obsidian-export
-git clone https://github.com/mssrprad/quartz
+# rm -rf obsidian-export
+# rm -rf quartz
+# git clone https://github.com/mssrprad/obsidian-export
+# git clone https://github.com/mssrprad/quartz
 cd quartz;
 npm i;
 rm -rf content/*;
 obsidian-export ~/Desktop/Obsidian-Vault/Pradu/ ./content --frontmatter=always --no-recursive-embeds;
 npx quartz build
-mv content/* ../new_website/
-mv public/* ../new_website/dist
+mkdir ../website
+mkdir ../website/dist
+mkdir ../website/content
+mv content/* ../website/content
+mv public/* ../website/dist
 npx quartz build --serve
--- new_website/content/Transformers/Kronecker Products.md
+++
@@ -1,8 +0,0 @@
----
----
-
-
- > 
- > \[!quote\] Tensor / Kronecker Product
- > ![Img](Transformers/images/Pasted%20image%2020240220144457.png)
- > ![Img](Transformers/images/Pasted%20image%2020240220144343.png)
